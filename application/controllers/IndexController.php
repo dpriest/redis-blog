a8deb208e8c71f18b3f12fbaa41c7471c5113928
dpriest <wenhaoz100@gmail.com>
2012-10-20 22:17:12 +0800

    add redis code 	modified:   application/configs/application.ini 	modified:   application/controllers/IndexController.php 	modified:   application/views/scripts/index/index.phtml 	application/models/

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,7 +10,13 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+    	$request = $this->getRequest();
+    	$page = $request->getParam('page')?$request->getParam('page'):1;
+    	$start = ($page-1)*10;
+    	$end = $page*10;
+        $redis = new Application_Model_Redis();
+        $redis->addserver('localhost');
+        $this->view->posts = $redis->lrange('posts', $start, $end);
     }
 
 
